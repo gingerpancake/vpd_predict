0c45391ef777b931c487d521eb84fb6cc8e2bfdf
karma <karma@DESKTOP-BC2I93N>
2026-08-08 21:34:15 +0900

    real-real base

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -160,15 +160,16 @@ int main(void)
   /* USER CODE END 2 */
 
   /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
   while (1)
-  {
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
-  }
-  /* USER CODE END 3 */
+    {
+  /* USER CODE BEGIN WHILE */
+	  //begin main
+  /* USER CODE END WHILE */
+    }
 }
+  /* USER CODE BEGIN 3 */
+
+  /* USER CODE END 3 */
 
 /**
   * @brief System Clock Configuration
