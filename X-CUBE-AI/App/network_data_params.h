afeee399bcc74562961d932b4a06abe18b102ae8
karma <karma@DESKTOP-BC2I93N>
2026-09-13 13:57:35 +0900

    main.c ver.8

--- X-CUBE-AI/App/network_data_params.h
+++ X-CUBE-AI/App/network_data_params.h
@@ -2,7 +2,7 @@
   ******************************************************************************
   * @file    network_data_params.h
   * @author  AST Embedded Analytics Research Platform
-  * @date    2026-09-13T13:19:38+0900
+  * @date    2026-09-13T13:57:03+0900
   * @brief   AI Tool Automatic Code Generator for Embedded NN computing
   ******************************************************************************
   * Copyright (c) 2026 STMicroelectronics.
